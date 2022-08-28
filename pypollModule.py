cc80cada62c518f925ffec548c46a53b2dcd149c
RynAgain <85427950+RynAgain@users.noreply.github.com>
2022-08-28 10:28:29 -0500

    Create pypollModule.py

---
+++ pypollModule.py
@@ -0,0 +1,17 @@
+#Total number of votes cast
+#A complete list of candidates who received votes
+#Total number of votes each candidate received
+#Percentage of votes each candidate won
+#The winner of the election based on popular vote
+
+#headers inclued Ballot_ID, County, Candidate
+
+#imports
+import
+
+#FUNCTIONS
+
+#open the data file
+
+
+#main code block
